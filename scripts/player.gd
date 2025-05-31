780704241205252fc2558c046bb4058b5686ad1b
KogaIJ <kogamianjimisnoname@gmail.com>
2025-05-31 15:54:17 +0300

    changed assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -16,9 +16,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
